449e4767a4dba5f00fe316492262bb21fda97bd7
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2017-12-23 20:07:34 +0100

    Added jQuery to dropdown menu

--- resources/js/designs.js
+++ resources/js/designs.js
@@ -1,4 +1,12 @@
 
 $(document).ready(function(){
 
-})
+    $(".submenu").on("mouseenter", function() {
+        $(this).find(".dropdown-submenu").toggleClass("active");
+    });
+
+    $(".submenu").on("mouseleave", function() {
+        $(this).find(".dropdown-submenu").toggleClass("active");
+    });
+
+});
